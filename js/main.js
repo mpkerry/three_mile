d349101552a029475dc7bf59bc7bbffff43e2f29
Michael Kerry <michaelpkerry@gmail.com>
2017-08-25 22:29:03 -0400

    faqs; carousel with captions

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(function () {
+    // set calendar
     $.get('/three_mile/data/calendar.json', function(data) {
         var calendar = data;
         $.each(calendar.weeks, function(key, week){
@@ -6,4 +7,26 @@ $(function () {
         });
         $('#calendar-year').html(calendar.year);
     });
+
+    // set FAQs
+    $.get('/three_mile/data/faq.json', function(data) {
+        var faqs = data;
+        var num = 0;
+        $.each(faqs, function(section, questions){
+            $('#faq-accordion-div').append('<br/><h4>' + section + '</h4>');
+            var accordion = $('<div>').attr('id','faq-accordion').attr('data-children', '.questions');
+            $.each(questions, function(q,a) {
+                accordion.append('<div class="questions">');
+                accordion.append('<a data-toggle="collapse" data-parent="#faq-accordion" href="#faq-accordion'+num+'" aria-expanded="false" aria-controls="faq-accordion'+num+'">'+ q + '? ' + '</a>');
+                accordion.append('<div id="faq-accordion'+num+'" class="collapse" role="tabpanel"><p class="mb-3">'+a + '</p></div></div>');
+                num++;
+            });
+            $('#faq-accordion-div').append(accordion);
+        });
+    });
+
+    // set year for copyright
+    var year = (new Date()).getFullYear();
+    $("#copyright-year").html(year);
 });
+
